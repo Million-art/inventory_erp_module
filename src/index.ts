3f7114abdfcfe625d147e46e0839cea9292f6140
million <74927604+Million-art@users.noreply.github.com>
2024-08-12 14:33:50 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Basic route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Inventory Management API');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
